df221426d4bf00199e496212d29561e4b454151a
Timmster <timm.schwemann@googlemail.com>
2022-05-12 09:28:58 +0200

    1 file modified

--- src/app/model/TcTournament.ts
+++ src/app/model/TcTournament.ts
@@ -8,17 +8,28 @@ export class TcTournament {
 
   constructor(public players: TcPlayer[], public teams: TcTeam[]) {}
 
+  initAllGames() {
+    this.initGames(TcGame.SACKEN);
+    this.initGames(TcGame.FIFA);
+    this.initGames(TcGame.KICKERN);
+    this.initGames(TcGame.LOOPING_LOUIE);
+    this.initGames(TcGame.MARIO_KART);
+    this.initGames(TcGame.TIPPKICK);
+  }
+
   initGames(game: TcGame) {
     if (game == TcGame.SACKEN) {
       this.matches = this.matches.filter((g) => g.game != game);
-      this.teams.forEach((home) => {
-        this.teams.forEach((away) => {
-          if (home.id != away.id) {
+
+      this.teams.forEach((home, index1: number) => {
+        this.teams.forEach((away, index2: number) => {
+          if (index2 > index1) {
+            const switchSides = (index2 + index1) % 2 == 1;
             this.matches.push(
               TcMatch.create1on1(
                 game,
-                home.getNextPlayer(),
-                away.getNextPlayer()
+                switchSides ? home.getNextPlayer() : away.getNextPlayer(),
+                switchSides ? away.getNextPlayer() : home.getNextPlayer()
               )
             );
           }
