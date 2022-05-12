269b891b0ed614686b1ecf616fb1f3eb03d07ff7
Timmster <timm.schwemann@googlemail.com>
2022-05-12 11:43:38 +0200

    1 file modified

--- src/app/model/TcTournament.ts
+++ src/app/model/TcTournament.ts
@@ -21,26 +21,32 @@ export class TcTournament {
     if (game == TcGame.SACKEN) {
       this.matches = this.matches.filter((g) => g.game != game);
       this.createGamesForDay(1, TcGame.SACKEN);
+      this.createGamesForDay(2, TcGame.SACKEN);
+      this.createGamesForDay(3, TcGame.SACKEN);
     }
   }
 
   createGamesForDay(day: number, game: TcGame) {
     const count = this.teams.length;
+    const half = count / 2;
     const rows = [];
     this.teams.forEach((team, index) => {
-      if (index == 0) {
-        rows.push(team);
-      } else {
+      if (index == 0 || index < half - day + 1 || index > count - day) {
         rows.push(team);
       }
     });
+    this.teams.forEach((team, index) => {
+      if (!rows.find((t) => t.id == team.id)) {
+        rows.splice(half, 0, team);
+      }
+    });
     rows.forEach((team, index) => {
-      if (index < count / 2) {
+      if (index < half) {
         this.matches.push(
           TcMatch.create1on1(
             game,
             team.getNextPlayer(),
-            rows[index + count / 2].getNextPlayer()
+            rows[index + half].getNextPlayer()
           )
         );
       }
