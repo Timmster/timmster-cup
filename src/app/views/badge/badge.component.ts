a2150a783f5a60b1e68f0bf688adc29f269bd5f6
Timmster <timm.schwemann@googlemail.com>
2022-05-12 12:35:08 +0200

    6 files modified

--- src/app/views/badge/badge.component.ts
+++ src/app/views/badge/badge.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { DATA } from '../../DB';
+import { TcPlayer } from '../../model/TcPlayer';
 
 @Component({
   selector: 'app-badge',
@@ -6,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./badge.component.css'],
 })
 export class BadgeComponent implements OnInit {
+  @Input()
+  player: TcPlayer;
+
   constructor() {}
 
   ngOnInit() {}
+
+  findTeam(s: TcPlayer) {
+    let team = null;
+    DATA.teams.forEach((t) => {
+      t.players.forEach((p) => {
+        if (p.id === s.id) {
+          console.log(p, s);
+          team = t;
+        }
+      });
+    });
+    return team;
+  }
 }
